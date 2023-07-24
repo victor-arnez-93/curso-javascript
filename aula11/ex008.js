16cf9a8bee53b0ca8d5bfec2772977bb9b1f30c0
Victor Arnez <victor_arnez@outlook.com>
2023-07-24 14:44:04 -0300

    atualizações

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 40
+console.log(`Sua velocidade é ${vel}km/h`)
+if (vel > '60'){
+    console.log('Você ultrapassou a velocidade e foi multado!')
+}
+console.log('Diriga sempre com o cinto de segurança!')
