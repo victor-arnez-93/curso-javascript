633cd782490cf559a440afdae4601d1ab61c82f7
Victor Arnez <victor_arnez@outlook.com>
2023-07-25 12:27:52 -0300

    atualizações

---
+++ aula 12 exercicios/hora do dia/script.js
@@ -0,0 +1,20 @@
+function carregar () {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas.`
+if (hora >= 0 && hora < 12) {
+    // BOM DIA
+    document.body.style.background = '#b3caed' //#427db6 #00518b
+    img.src = 'imagens/dia.png'
+} else if (hora >= 12 && hora <= 18) {
+    // BOA TARDE
+    document.body.style.background = '#6e87a6' //#da8350 #0a101c
+    img.src = 'imagens/tarde.png'
+} else {
+    // BOA NOITE
+    document.body.style.background = '#9374ad' //#382b47 #0d0609
+    img.src = 'imagens/noite.png'
+    }
+}
